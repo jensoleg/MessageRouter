3ee48a844562f76f6894f4c4756583d80bc3674c
jensolegraulund <jensole@graulund.net>
2015-06-08 23:46:30 +0200

    Removed login config and sonsol logs

--- app.js
+++ app.js
@@ -19,38 +19,34 @@ if (process.env.MQTT_PORT_1883_TCP_ADDR) {
     config.mqtt.port = process.env.MQTT_PORT_1883_TCP_PORT;
 }
 
-console.log('MQTT options: ', config.mqtt.options);
-
 var client = mqtt.connect('mqtt://' + config.mqtt.host + ':' + config.mqtt.port, config.mqtt.options),
     deviceConnection = mongoose.createConnection(mongooseDeviceUri, config.mongodb.options),
     triggers = new Triggers(deviceConnection),
     installations = new Installations(deviceConnection, config.domain + '.installation'),
     subscriptions = [];
 
-console.log('alarmrouter started', config);
-
 installations.allTrigger(function (error, result) {
-    console.log('subscriptions: ', result);
+    console.info('subscriptions: ', result);
     subscriptions = result;
 });
 
 client.on('connect', function () {
     _.each(subscriptions, function (sub) {
         client.subscribe('/' + config.domain + '/' + sub.deviceId + '/' + sub.control);
-        console.log('subscribed to : ', '/' + config.domain + '/' + sub.deviceId + '/' + sub.control);
+        console.info('subscribed to : ', '/' + config.domain + '/' + sub.deviceId + '/' + sub.control);
     });
     console.log('connected');
 });
 
 client.on('error', function () {
-    console.log('connect failed');
+    console.info('connect failed');
 });
 
 client.on('close', function () {
-    console.log('client disconnetced');
+    console.info('client disconnetced');
 });
 
 client.on('message', function (topic, message) {
-    console.log('Event Published: ' + topic + ' Message :' + message);
+    console.info('Event Published: ' + topic + ' Message :' + message);
     triggers.handle(topic, message);
 });
